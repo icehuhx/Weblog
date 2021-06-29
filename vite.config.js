f0cb341bd9b486a40f8783bab4ace78658246a98
胡惠璇 <huhuixuan@huhuixuandeMac-mini.local>
2021-06-29 15:59:06 +0800

    base path

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
         '@': path.resolve(__dirname, './src')
     }  
-  },
-  build: {
-    outDir: 'dist',
-    assetsRoot: path.resolve(__dirname, './dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: './'
   }
 })
